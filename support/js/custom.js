46914a481acbf22edc8aa4471a11440a4e183ece
junpengzhou <junpengzhou@users.noreply.github.com>
2023-05-19 07:45:10 +0000

    deploy: 7b953e4ec5565b3b5a0dd56949a1be25bf5ab35e

--- support/js/custom.js
+++ support/js/custom.js
@@ -10,11 +10,6 @@ if (mobile_sidebar_menus) {
     }
 }
 
-/** 监控调试事件 */
-document.onkeydown = function (e) {
-    if (e.keyCode === 123 || "F12" == e.code || (e.ctrlKey && e.shiftKey && ("KeyI" === e.code || "KeyJ" === e.code || "KeyC" === e.code)) || (e.ctrlKey && "KeyU" === e.code)) return btf.snackbarShow("本站装有监控，禁止非法调试！"), event.keyCode = 0, event.returnValue = !1, !1
-};
-
 /** 监听copy事件 */
 document.addEventListener("copy", function (e) {
     //复制的内容
