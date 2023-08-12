03847ad322a9ee6c3870d4168a58ca12772b3fd4
junpengzhou <junpengzhou@users.noreply.github.com>
2023-08-12 09:15:16 +0000

    deploy: 82b9c7a6de0433a3e2982cd0c4fcec5d288d2f31

--- support/js/custom.js
+++ support/js/custom.js
@@ -5,7 +5,7 @@ if (mobile_sidebar_menus) {
         "menus_item_child"
     );
     var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-    for (var i = 0; i < menus_item_child.length; i++) {
+    for (var i = 0; i &lt; menus_item_child.length; i++) {
         menus_item_child[i].style.display = "none";
     }
 }
