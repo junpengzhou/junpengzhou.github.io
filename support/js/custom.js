58f3cb51718a4e9a7123740d840ac8ecc74ce93f
junpengzhou <junpengzhou@users.noreply.github.com>
2023-08-30 04:18:27 +0000

    deploy: 5c00c47a04b2e12debe1a389f82d682bd89dd519

--- support/js/custom.js
+++ support/js/custom.js
@@ -1,17 +1,1 @@
-/* 手机侧边栏默认不展开 */
-var mobile_sidebar_menus = document.getElementById("sidebar-menus");
-if (mobile_sidebar_menus) {
-    var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-        "menus_item_child"
-    );
-    var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-    for (var i = 0; i &lt; menus_item_child.length; i++) {
-        menus_item_child[i].style.display = "none";
-    }
-}
-
-/** 监听copy事件 */
-document.addEventListener("copy", function (e) {
-    //复制的内容
-    btf.snackbarShow('复制成功，请遵循GPL协议', false, 3000)
-})
+var mobile_sidebar_menus=document.getElementById("sidebar-menus");if(mobile_sidebar_menus)for(var menus_item_child=mobile_sidebar_menus.getElementsByClassName("menus_item_child"),menus_expand=mobile_sidebar_menus.getElementsByClassName("expand"),i=0;i<menus_item_child.length;i++)menus_item_child[i].style.display="none";document.addEventListener("copy",(function(e){btf.snackbarShow("复制成功，请遵循GPL协议",!1,3e3)}));
